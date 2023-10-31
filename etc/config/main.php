bfc7c9ec9da7f8d4494f6773aba28ca4e5c4b5bf
bagas dicko <bagas@hris-system.com>
2023-10-31 10:31:10 +0700

    init

---
+++ etc/config/main.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'name' => 'Admin Wisata',
+    'version' => '0.0.1',
+    'host' => 'admin.wisata.test',
+    'timezone' => 'Asia/Jakarta',
+    'install' => '2023-10-31 10:08:03',
+    'secure' => FALSE,
+    '__gitignore' => [
+        'modules/*' => NULL,
+        '!modules/.gitkeep' => NULL
+    ]
+];
---
+++ etc/modules.php
@@ -0,0 +1,7 @@
+<?php
+/* GENERATE BY CLI */
+/* DON'T MODIFY */
+
+return [
+    'core' => 'https://github.com/getmim/core.git'
+];
